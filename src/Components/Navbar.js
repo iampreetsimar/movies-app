5f1873b62632c8b49bdf08891fb768d97cc644a6
Simarpreet Singh <iampreetsimar@gmail.com>
2025-02-21 00:05:27 +0530

    updated style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
   render() {
     return (
-        <div style={{ display:'flex', background:'DarkGoldenRod', padding:'0.5rem'}}>
+        <div style={{ display:'flex', background:'white', padding:'0.5rem'}}>
             <Link to='/' style={{ textDecoration: 'none' }}><h1>Movies App</h1></Link>
             <Link to='/favs' style={{ textDecoration: 'none' }}><h2 style={{ marginLeft:'3rem', marginTop:'0.4rem' }}>Favorites</h2></Link> 
         </div>
